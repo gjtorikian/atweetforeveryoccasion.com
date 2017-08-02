048de6060118c537290b1f767a266c624fe1c796
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-08-02 01:14:29 -0400

    Drop anchors in tweets They mess up the layout

--- script/validate_entries.rb
+++ script/validate_entries.rb
@@ -8,6 +8,10 @@ entries.each_with_index do |entry, i|
   if entry.keys.sort != VALID_ENTRY_KEYS
     raise("`entry.keys` at index #{i} is missing: #{VALID_ENTRY_KEYS - entry.keys}")
   end
+
+  if tweet =~ /a href/
+    raise("Please remove the `anchor` tag at index #{i}")
+  end
 end
 
 puts 'Entries looks good! 👍'
